bdcfceedd928cce88278e7db24acca99b11ce857
Kovacs Áron László <kovacs.aron@arteries.hu>
2020-07-17 14:43:56 +0200

    add new method to chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
     this.room = room;
     this.username = username;
     this.chats = db.collection("chats");
+    this.unsub;
   }
 
   async addChat(message) {
@@ -24,8 +25,10 @@ class Chatroom {
     return response;
   }
 
-  getChat(callback) {
-    this.chats
+  getChats(callback) {
+    this.unsub = this.chats
+      .where("room", "==", this.room)
+      .orderBy("created_at")
       .onSnapshot(snapshot => {
         snapshot.docChanges().forEach(change => {
           if(change.type === "added") {
@@ -34,10 +37,29 @@ class Chatroom {
         })
       })
   }
+
+  updateName(username) {
+    this.username = username;
+  }
+
+  updateRoom(room) {
+    this.room = room;
+    console.log("room updated");
+    if (this.unsub) {
+      this.unsub();
+    }
+  }
 }
 
-const chatroom = new Chatroom("gaming", "shaun");
+const chatroom = new Chatroom("general", "shaun");
 
-chatroom.getChat(data => {
+chatroom.getChats(data => {
   console.log(data)
 });
+
+setTimeout(() => {
+  chatroom.updateRoom('general');
+  chatroom.updateName('yoshi');
+  chatroom.getChats(data => console.log(data));
+  chatroom.addChat('hello');
+}, 3000);
